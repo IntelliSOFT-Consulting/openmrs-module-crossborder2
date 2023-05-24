0ca24d07cbd22416d70c69f65d2ecac3cab8ad65
moshonk <omondi.ken@gmail.com>
2023-05-24 10:42:32 +0300

    Updated National ID uuid reference

--- api/src/main/java/org/openmrs/module/crossborder2/CbConstants.java
+++ api/src/main/java/org/openmrs/module/crossborder2/CbConstants.java
@@ -7,15 +7,15 @@ public class CbConstants {
 	public static final String PROP_MPI_SERVER_USERNAME = "crossborder.mpiServerUsername";
 	
 	public static final String PROP_MPI_PASSWORD = "crossborder.mpiServerPassword";
-
+	
 	public static final class _PatientIdentifierType {
-
-		public static final String NATIONAL_ID = "f85081e2-b4be-4e48-b3a4-7994b69bb101";
-
+		
+		public static final String NATIONAL_ID = "49af6cdc-7968-4abb-bf46-de10d7f4859f";
+		
 		public static final String BIRTH_CERTIFICATE_NUMBER = "68449e5a-8829-44dd-bfef-c9c8cf2cb9b2";
-
+		
 		public static final String NATIONAL_UNIQUE_PATIENT_IDENTIFIER = "f85081e2-b4be-4e48-b3a4-7994b69bb101";
-
+		
 		public static final String CROSS_BORDER_IDENTIFIER_UUID = "e5e9a994-12e2-42c3-9c02-5abdc0fe40e8";
 	}
 }
