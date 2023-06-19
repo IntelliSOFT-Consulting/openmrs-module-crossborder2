351e92c69303983c1133de3f9e7ab6c37d914b39
moshonk <omondi.ken@gmail.com>
2023-06-19 14:26:20 +0300

    Install CB role & privileges

--- api/src/main/java/org/openmrs/module/crossborder2/metadata/CbMetadata.java
+++ api/src/main/java/org/openmrs/module/crossborder2/metadata/CbMetadata.java
@@ -1,10 +1,10 @@
 package org.openmrs.module.crossborder2.metadata;
 
+import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.*;
+
 import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.openmrs.module.metadatadeploy.bundle.AbstractMetadataBundle;
 import org.springframework.stereotype.Component;
-
-import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.form;
 
 @Component
 public class CbMetadata extends AbstractMetadataBundle {
@@ -13,10 +13,26 @@ public class CbMetadata extends AbstractMetadataBundle {
 	
 	private static final String CB_REFERRAL_FORM_UUID = "01839000-c275-11ed-9fc5-0b220ae9bf59";
 	
+	public static class _Privilege {
+		
+		public static final String APP_CROSS_BORDER_ADMIN = "App: crossborder2.app.cb";
+	}
+	
+	public static final class _Role {
+		
+		public static final String APPLICATION_CROSS_BORDER_ADMIN = "Cross-Border app administration";
+	}
+	
 	@Override
 	public void install() throws Exception {
 		install(form("Cross Border Screening", null, HivMetadata._EncounterType.HIV_CONSULTATION, "1",
 		    CB_SCREENING_FORM_UUID));
 		install(form("Cross Border Referral", null, HivMetadata._EncounterType.HIV_CONSULTATION, "1", CB_REFERRAL_FORM_UUID));
+		
+		install(privilege(_Privilege.APP_CROSS_BORDER_ADMIN, "Able to access Cross-Border App"));
+		
+		install(role(_Role.APPLICATION_CROSS_BORDER_ADMIN, "Can access Cross-Border app",
+		    idSet(org.openmrs.module.kenyaemr.metadata.SecurityMetadata._Role.API_PRIVILEGES_VIEW_AND_EDIT),
+		    idSet(_Privilege.APP_CROSS_BORDER_ADMIN)));
 	}
 }
